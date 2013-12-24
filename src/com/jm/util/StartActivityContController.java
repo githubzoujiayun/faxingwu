b1ba55323774463eea88df951f2c8b2f36b45ffa
Android <233061987@qq.com>
2013-12-24 16:10:34 +0800

    activity跳转控制

--- src/com/jm/util/StartActivityContController.java
+++ src/com/jm/util/StartActivityContController.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 
 import com.jm.fxw.LoginUI;
+import com.jm.fxw.PublicPortfolio;
+import com.jm.fxw.PublicPortfolioOfHair;
 import com.jm.fxw.QingBaoUI_Haier;
 import com.jm.fxw.QingBaoUI_User;
 import com.jm.fxw.WodeUI_Hairer;
@@ -47,13 +49,20 @@ public class StartActivityContController {
 		}
 	}
 
+	/**
+	 * 判断登录/跳转
+	 * @param c 当前activity
+	 * @param cls 需要跳转的activity
+	 * @param checklogin 是否需要验证登录
+	 * @param hm Map传值
+	 */
 	public static void goPage(Context c, Class<?> cls, boolean checklogin,
 			Map<String, String> hm) {
 		Intent i = new Intent(c, cls);
 
 		for (String key : hm.keySet()) {
 			i.putExtra(key, hm.get(key).toString());
-			LogUtil.e("Intent: key=" + key + "|value=" + hm.get(key).toString());
+			LogUtil.i("Intent: key=" + key + "|value=" + hm.get(key).toString());
 		}
 		if (checklogin) {
 			sm = SessionManager.getInstance();
@@ -92,38 +101,47 @@ public class StartActivityContController {
 		Intent in = new Intent();
 		if (101 == intent) {
 			if ("1".equals(sm.getUsertype())) {
-				LogUtil.e("跳转个人中心");
+				LogUtil.i("跳转个人中心");
 				return in.setClass(c, WodeUI_User.class);
 			} else if ("2".equals(sm.getUsertype())) {
-				LogUtil.e("跳转发型师中心");
+				LogUtil.i("跳转发型师中心");
 				return in.setClass(c, WodeUI_Hairer.class);
 			}
 		}
 		if (102 == intent) {
 			if ("1".equals(sm.getUsertype())) {
-				LogUtil.e("跳转个人预约页面");
+				LogUtil.i("跳转个人预约管理页面");
 				return in.setClass(c, YuYueUI_User.class);
 			} else if ("2".equals(sm.getUsertype())) {
-				LogUtil.e("跳转发型师预约");
+				LogUtil.i("跳转发型师管理预约");
 				return in.setClass(c, YuYueUI_Haier.class);
 			}
 		}
 		if (103 == intent) {
 			if ("1".equals(sm.getUsertype())) {
-				LogUtil.e("跳转个人预约页面");
+				LogUtil.i("跳转个人预约详情页面");
 				return in.setClass(c, YuYueInfoUI_User.class);
 			} else if ("2".equals(sm.getUsertype())) {
-				LogUtil.e("跳转发型师预约");
+				LogUtil.i("跳转发型师预约详情页面");
 				return in.setClass(c, YuYueInfoUI_Haier.class);
 			}
 		}
 		if (104 == intent) {
 			if ("1".equals(sm.getUsertype())) {
-				LogUtil.e("跳转个人预约页面");
+				LogUtil.i("用户情报");
 				return in.setClass(c, QingBaoUI_User.class);
 			} else if ("2".equals(sm.getUsertype())) {
-				LogUtil.e("跳转发型师预约");
+				LogUtil.i("发型师情报");
 				return in.setClass(c, QingBaoUI_Haier.class);
+			}
+		}
+		if (105 == intent) {
+			if ("1".equals(sm.getUsertype())) {
+				LogUtil.i("用户上传发型页面");
+				return in.setClass(c, PublicPortfolio.class);
+			} else if ("2".equals(sm.getUsertype())) {
+				LogUtil.i("发型师上传发型页面");
+				return in.setClass(c, PublicPortfolioOfHair.class);
 			}
 		}
 		return in;
